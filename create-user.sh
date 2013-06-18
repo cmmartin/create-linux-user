2ad79d1ed01bacf1a6b6f842615100adffe35abb
Charlie <cmmartin24@gmail.com>
2013-06-18 16:01:05 +0000

    initial commit

---
+++ create-user.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ROOT_UID=0     # Only users with $UID 0 have root privileges.
+E_NOTROOT=87   # Non-root exit error.
+
+# Run as root only
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+  echo "You need root priveledges to run this script"
+  exit $E_NOTROOT
+fi
+
+echo -n "### Enter new user name: "
+read NEW_USER
+adduser $NEW_USER
+echo -n "### make new user a sudoer? (y/n) "
+read YES
+case ${YES} in
+y* )
+adduser $NEW_USER sudo
+;;
+* )
+continue
+;;
+esac
+
+su $NEW_USER
+
+echo "Done. You are now `whoami`"
+
+exit 0
