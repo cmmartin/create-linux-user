bc75c421796d90b07cf1f1cd60bc24ee5949dde4
Charlie Martin <cmmartin@users.noreply.github.com>
2016-11-17 17:46:36 -0500

    in my older, wiser years I dont think this script needs to exist but whatev

--- create-user.sh
+++ create-user.sh
@@ -3,7 +3,7 @@
 ROOT_UID=0     # Only users with $UID 0 have root privileges.
 E_NOTROOT=87   # Non-root exit error.
 
-# Run as root only
+# Run as root only (sudo counts)
 if [ "$UID" -ne "$ROOT_UID" ]
 then
   echo "You need root priveledges to run this script"
